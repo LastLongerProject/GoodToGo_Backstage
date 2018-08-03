73ceefc083f05dedfee37fed0acb07b26b9c177f
Benjamin Chen <s910190benjamin@gmail.com>
2018-08-03 22:32:46 +0800

    finish demo

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", function(window, event) {
-    console.log("done")
+    var navLink_nowActive = $(".mdl-navigation__link.is-active");
+    var navLinks = $(".mdl-navigation__link");
+    var content_nowActive = $(".main-content.is-active");
+    navLinks.click(function navLinkClickListener() {
+        navLink_nowActive.removeClass('is-active');
+        $(this).addClass('is-active');
+        navLink_nowActive = $(this);
+        content_nowActive.removeClass('is-active');
+        content_nowActive = $(navLink_nowActive.attr('href'));
+        content_nowActive.addClass('is-active');
+        $('.mdl-layout__obfuscator.is-visible').click();
+    });
 });
