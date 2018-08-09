f381ea1e16f5bee3f91b4d5a4322513f63ff0f2a
Benjamin Chen <s910190benjamin@gmail.com>
2018-08-09 21:20:26 +0800

    20180809

--- src/js/api.js
+++ src/js/api.js
@@ -1,38 +1,49 @@
-function requestData(page, cb) {
-    startLoading();
-    var requestUrl = "/manager/data/" + page;
-    // $.ajax(requestUrl, {
-    //         dataType: "json"
-    //     })
-    //     .done(function(data, textStatus, jqXHR) {
-    //         stopLoading();
-    //         cb(data);
-    //     })
-    //     .fail(function(jqXHR, textStatus, errorThrown) {
-    //         stopLoading();
-    //         showErr(jqXHR.statusText);
-    //     })
-    //     .always(function() {});
-    console.log(requestUrl);
-    setTimeout(function() {
-        stopLoading();
-        cb();
-    })
-}
+(function(window) {
+    var pendingReq = null;
 
-function startLoading() {
-    app.loading.txt = "Loading...";
-    app.loading.show = true;
-}
+    function requestData(page, cb) {
+        // if(pendingReq) pendingReq.abort();
+        if (pendingReq) clearTimeout(pendingReq);
+        startLoading();
+        var requestUrl = "/manager/data/" + page;
+        // pendingReq = $.ajax(requestUrl, {
+        //         dataType: "json"
+        //     })
+        //     .done(function(data, textStatus, jqXHR) {
+        //         stopLoading();
+        //         cb(data);
+        //     })
+        //     .fail(function(jqXHR, textStatus, errorThrown) {
+        //         stopLoading();
+        //         showErr(jqXHR.statusText);
+        //     })
+        //     .always(function() {
+        //         pendingReq = null;
+        //     });
+        console.log(requestUrl);
+        pendingReq = setTimeout(function() {
+            pendingReq = null;
+            stopLoading();
+            cb();
+        }, 1000);
+    }
 
-function stopLoading() {
-    app.loading.show = false;
-    app.loading.txt = "";
-}
+    function startLoading() {
+        app.loading.txt = "Loading...";
+        app.loading.show = true;
+    }
 
-function showErr(errMsg) {
-    document.querySelector('#toast').MaterialSnackbar.showSnackbar({
-        message: errMsg,
-        timeout: 4000
-    });
-}
+    function stopLoading() {
+        app.loading.show = false;
+        app.loading.txt = "";
+    }
+
+    function showErr(errMsg) {
+        document.querySelector('#toast').MaterialSnackbar.showSnackbar({
+            message: errMsg,
+            timeout: 4000
+        });
+    }
+
+    window.requestData = requestData;
+}(window));
